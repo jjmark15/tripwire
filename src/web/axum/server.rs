b41ffce10db0dcc6d36b6639a370e8dbc7827b2d
Josh Jones <ohblonddev@gmail.com>
2022-11-05 14:14:37 +0000

    Add signature to headers

--- src/web/axum/server.rs
+++ src/web/axum/server.rs
@@ -1,7 +1,8 @@
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use axum::http::{HeaderValue, StatusCode};
+use axum::response::{IntoResponse, Response};
 use axum::routing::{any, get};
 use axum::Router;
+use tower::ServiceBuilder;
 
 use crate::web::axum::echo::echo;
 
@@ -10,6 +11,17 @@ pub(crate) fn axum_server() -> Router {
         .route("/", get(hi))
         .route("/echo/*path", any(echo))
         .route("/echo", any(echo))
+        .layer(ServiceBuilder::new().map_response(add_signature))
+}
+
+fn add_signature(mut response: Response) -> Response {
+    response
+        .headers_mut()
+        .insert("wizard", HeaderValue::from_str("ClumsyWizard").unwrap());
+    response
+        .headers_mut()
+        .insert("service", HeaderValue::from_str("tripwire").unwrap());
+    response
 }
 
 async fn hi() -> impl IntoResponse {
